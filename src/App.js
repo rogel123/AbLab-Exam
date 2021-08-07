7b5ab42bd0c5268d74b688b7d0db7050bf693671
rogel123 <rogelvelasco123@gmail.com>
2021-08-07 23:50:25 +0800

    Initial push

--- src/App.js
+++ src/App.js
@@ -1,22 +1,149 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { TextField } from "./components";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button, CircularProgress } from "@material-ui/core/";
+import axios from "axios";
+import Skeleton from "@material-ui/lab/Skeleton";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: "30ch",
+    },
+    background: "rgba(255,255,255,0.8)",
+    borderRadius: 10,
+    padding: 50,
+    marginTop: 20,
+    display: "flex",
+    flexDirection: "column",
+    minWidth: 500,
+  },
+  header: {
+    fontSize: 32,
+    fontWeight: "700",
+  },
+  subHeader: {
+    fontSize: 24,
+  },
+}));
 
 function App() {
+  const [data, setData] = useState();
+  const [error, setError] = useState();
+  const [responseJson, setResponseJson] = useState();
+  const [loading, setLoading] = useState();
+
+  const classes = useStyles();
+
+  useEffect(() => {
+    axios
+      .get("https://vb-react-exam.netlify.app/api/form")
+      .then((res) => setData(res.data.data));
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let err = [];
+    let postData = {};
+    data.map((e) => {
+      if (e.value === "") {
+        err.push("error");
+      }
+      postData = { ...postData, [e.fieldName]: e.value };
+      return "";
+    });
+
+    if (err.length === 0 && !error) {
+      axios
+        .post("https://vb-react-exam.netlify.app/api/form", postData)
+        .then((res) => {
+          setResponseJson(JSON.stringify(res.data));
+          setLoading(false);
+        });
+      setLoading(true);
+    } else {
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div className={classes.header}>AB Labs Solution</div>
+        <div className={classes.subHeader}>React Exam - by: Rogel Velasco</div>
+        <form
+          className={classes.root}
+          noValidate
+          autoComplete="off"
+          onSubmit={handleSubmit}
         >
-          Learn React
-        </a>
+          {data ? (
+            data.map((field, index) => {
+              let val = field.value;
+              return (
+                <TextField
+                  key={field.fieldName}
+                  value={val}
+                  label={field.fieldName}
+                  options={field.options}
+                  inputType={field.type}
+                  onChangeFunc={(e) => {
+                    val = e;
+                    data[index].value = val;
+                  }}
+                  errorVal={(e) => setError(e)}
+                />
+              );
+            })
+          ) : (
+            <div>
+              <Skeleton />
+              <Skeleton />
+              <Skeleton />
+              <Skeleton />
+              <Skeleton />
+            </div>
+          )}
+          <Button
+            variant="contained"
+            type="submit"
+            style={{
+              marginInline: 8,
+              background: "#039be5",
+              color: "#fff",
+              marginTop: 10,
+            }}
+          >
+            SUBMIT
+          </Button>
+          {loading && (
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                marginTop: 20,
+              }}
+            >
+              <CircularProgress color="#039be5" />
+            </div>
+          )}
+          {responseJson && (
+            <div
+              style={{
+                fontSize: 14,
+                maxWidth: 500,
+                wordWrap: "break-word",
+                marginTop: 20,
+              }}
+            >
+              {responseJson}
+            </div>
+          )}
+        </form>
       </header>
     </div>
   );
---
+++ src/components/TextField.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import MenuItem from "@material-ui/core/MenuItem";
+
+const TextFields = ({
+  value,
+  label,
+  inputType,
+  options,
+  onChangeFunc,
+  errorVal,
+}) => {
+  const [defaultData, setData] = React.useState(value);
+  const [errors, setError] = React.useState("");
+
+  const handleChange = (event) => {
+    if (inputType === "email") {
+      if (
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+          event.target.value
+        )
+      ) {
+        setError(null);
+        errorVal(null);
+      } else {
+        setError("Invalid");
+        errorVal("Invalid");
+      }
+    }
+
+    if (event.target.value === "") {
+      setError("Invalid");
+    } else {
+      if (inputType !== "email") setError(null);
+    }
+    console.log(inputType);
+
+    setData(event.target.value);
+    onChangeFunc(event.target.value);
+  };
+
+  const displayLabel = () => {
+    return label === "firstName"
+      ? "First Name"
+      : label === "lastName"
+      ? "Last Name"
+      : label === "emailAddress"
+      ? "Email Address"
+      : label;
+  };
+  return (
+    <TextField
+      id={`input-${label}`}
+      size="small"
+      value={defaultData}
+      label={displayLabel()}
+      variant="outlined"
+      multiline={inputType === "multiline" ? true : false}
+      rows={6}
+      fullWidth
+      select={inputType === "select" ? true : false}
+      onChange={handleChange}
+      error={errors ? true : false}
+      helperText={
+        errors ? `${displayLabel()} is required or Invalid Format` : ""
+      }
+    >
+      {options &&
+        options.map((option) => (
+          <MenuItem key={option} value={option}>
+            {option}
+          </MenuItem>
+        ))}
+    </TextField>
+  );
+};
+
+export default TextFields;
